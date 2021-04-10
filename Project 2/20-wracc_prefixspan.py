35fa808b137dbe2163c317a0aef95bd43a17f6c1
Romain Graux <romain.graux@student.uclouvain.be>
2021-04-10 23:14:25 +0200

    First slow working version of wracc prefixspan

---
+++ Project 2/20-wracc_prefixspan.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+@author : Romain Graux
+@date : 2021 Apr 09, 16:22:38
+@last modified : 2021 Apr 10, 23:09:07
+"""
+
+
+import importlib
+from utils import IO, get_negative_positive_support
+sumsup_prefixspan = importlib.import_module("10-sumsup_prefixspan")
+PrefixSpan = sumsup_prefixspan.PrefixSpan
+Dataset = sumsup_prefixspan.Dataset
+
+
+class WraccPrefixSpan(PrefixSpan):
+    def _get_score_key(self, matches):
+        n, p = get_negative_positive_support(
+            self._dataset, matches
+        )  # Get negative and positive support of `matches`
+        N, P = (
+            self._dataset.n_negative,
+            self._dataset.n_positive,
+        )  # Get the total number of negative and positive transactions
+
+        positive_part = N * p / (N + P) ** 2
+        negative_part = P * n / (N + P) ** 2
+        wracc = positive_part - negative_part
+
+        # Get at least one occurence in the dataset
+        if p == 0:
+            positive_part = -negative_part / n
+
+        return round(positive_part, 5), round(wracc, 5)
+
+
+if __name__ == "__main__":
+    import cProfile, pstats
+    args = IO.from_stdin()
+    ds = Dataset(args.negative_filepath, args.positive_filepath)
+    algo = WraccPrefixSpan(ds)
+    if args.cprofile:
+        profiler = cProfile.Profile()
+        profiler.enable()
+        results = algo(args.k)
+        profiler.disable()
+        stats = pstats.Stats(profiler).sort_stats("cumtime")
+        stats.print_stats()
+    else:
+        results = algo(args.k)
---
+++ Project 2/utils.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author : Romain Graux
+@date : 2021 Apr 10, 11:50:37
+@last modified : 2021 Apr 10, 23:02:30
+"""
+
+
+def get_negative_positive_support(dataset, matches):
+    from bisect import bisect_left
+
+    positive_support = bisect_left(
+        matches, (dataset.n_positive, -1)
+    )  # Get the index of the maximum positive index in the matches
+    negative_support = len(matches) - positive_support
+    return negative_support, positive_support
+
+
+class IO:
+    @staticmethod
+    def output(dataset, results, fd):
+        for support, pattern, matches in results:
+            n, p = get_negative_positive_support(dataset, matches)
+            items = [dataset._int_to_item[integer] for integer in pattern]
+
+            fd.write(f'[{", ".join(items)}] {p} {n} {support}\n')
+
+    @staticmethod
+    def to_stdout(dataset, results):
+        import sys
+
+        IO.output(dataset, results, sys.stdout)
+
+    @staticmethod
+    def to_file(dataset, results, filepath):
+        fd = open(filepath, "w+")
+        IO.output(dataset, results, fd)
+
+    @staticmethod
+    def from_stdin():
+        from argparse import ArgumentParser
+
+        parser = ArgumentParser()
+        parser.add_argument(
+            "positive_filepath", help="Path to the positive file", type=str
+        )
+        parser.add_argument(
+            "negative_filepath", help="Path to the negative file", type=str
+        )
+        parser.add_argument("k", help="The number of top sequential patterns", type=int)
+        parser.add_argument("-c", "--cprofile", help="Run the cprofiler", type=bool, default=False)
+
+        args = parser.parse_args()
+        return args
