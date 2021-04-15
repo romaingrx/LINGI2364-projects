0eccc2d1310a91e466c97982c932fe1bf112f81b
Romain Graux <romain.graux@student.uclouvain.be>
2021-04-15 15:43:45 +0200

    Clean benchmark

--- Project 2/benchmark.sh
+++ Project 2/benchmark.sh
@@ -22,12 +22,20 @@ i=1
 for pyfile in ${PY_FILES}; do
     for dataset in {0..2}; do 
         for k in $(seq 1 $K); do
+
             tic=$(date +%s.%N)
+            # Count the number of top k
             ntopk=$(./run_algo_dataset ${pyfile} ${dataset} ${k} 0 | wc -l)
             tac=$(date +%s.%N)
-            taken_time=$(echo "$(date +%s.%N) - $tic" | bc)
+
+            # Compute the taken time
+            taken_time=$(echo "$tac - $tic" | bc)
+
+            # Output the line in the csv file
             csv_line="$pyfile,$dataset,$k,$ntopk,$taken_time"
             echo $csv_line >> ${OUTPUT_CSV}
+
+            # Log
             echo -ne "$i/$N_TOTAL :: $(echo $csv_line | column -t -s ",")\r"
             i=$(($i+1))
         done
