359d2acaf2fd51a30ad65a95d5a293143cf36e86
Romain Graux <romain.graux@student.uclouvain.be>
2021-04-15 15:39:19 +0200

    Fix benchmark

---
+++ Project 2/benchmark.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+OUTPUT_CSV="benchmark.csv"
+HEADER_CSV="pyfile,dataset,k,number_top_k,time"
+PY_FILES=$(ls | grep "0[1-9]1*.*.py")
+N_PYFILES=$(ls | grep "0[1-9]1*.*.py" | wc -l)
+
+if [ $# -eq 1 ]; then 
+    K=$1
+else
+    K=30
+fi
+
+N_TOTAL=$(($N_PYFILES*$K*3))
+
+# First output the header of the csv
+echo ${HEADER_CSV} > ${OUTPUT_CSV}
+echo -ne "          "
+echo "${HEADER_CSV}" | column -t -s ","
+
+i=1
+for pyfile in ${PY_FILES}; do
+    for dataset in {0..2}; do 
+        for k in $(seq 1 $K); do
+            tic=$(date +%s.%N)
+            ntopk=$(./run_algo_dataset ${pyfile} ${dataset} ${k} 0 | wc -l)
+            tac=$(date +%s.%N)
+            taken_time=$(echo "$(date +%s.%N) - $tic" | bc)
+            csv_line="$pyfile,$dataset,$k,$ntopk,$taken_time"
+            echo $csv_line >> ${OUTPUT_CSV}
+            echo -ne "$i/$N_TOTAL :: $(echo $csv_line | column -t -s ",")\r"
+            i=$(($i+1))
+        done
+    done
+done
